99e0f231f62da658549e1d891c94b95a8e19631d
Derek Enos <derek.enos@gmail.com>
2020-12-20 12:24:42 -0500

    Replace test machinery with testy submodule and fixed tests to work with new EOF return value instead of StopIteration.

--- test.py
+++ test.py
@@ -1,33 +1,15 @@
 
-from io import BytesIO
-from sys import stdout
+from testy import (
+    assertEqual,
+    cli,
+)
 
 from __init__ import (
+    EOF,
     REPLACE,
     InvalidUTF8Encoding,
     UTF8Decoder,
 )
-
-###############################################################################
-# Testing helpers
-###############################################################################
-
-class Skip(Exception): pass
-class DidNotRaise(Exception): pass
-
-def _assertEqual(result, expected):
-    if result != expected:
-        raise AssertionError(
-            f'Expected ({repr(expected)}), got ({repr(result)})'
-        )
-
-def _assertRaises(exc, fn, *args, **kwargs):
-    try:
-        fn(*args, **kwargs)
-    except exc:
-        pass
-    else:
-        raise DidNotRaise
 
 ###############################################################################
 # Tests
@@ -37,10 +19,13 @@ def test_stress_test():
     # Test processing the UTF-8 decoder capability and stress test from:
     # https://www.cl.cam.ac.uk/~mgk25/ucs/examples/UTF-8-test.txt
     input_fh = open('test_data/UTF-8-test.txt', 'rb')
-    expected_fh = open('test_data/UTF-8-test.txt.expected', 'r', encoding='utf-8')
+    expected_fh = open('test_data/UTF-8-test.txt.expected', 'r',
+                       encoding='utf-8')
     decoder = UTF8Decoder(input_fh, errors=REPLACE)
     for c in decoder:
-        _assertEqual(c, expected_fh.read(1))
+        if c == EOF:
+            break
+        assertEqual(c, expected_fh.read(1))
 
 def test_i_can_eat_glass():
     # Test processing the "I CAN EAT GLASS" section at:
@@ -49,29 +34,11 @@ def test_i_can_eat_glass():
     decoder_fh = open('test_data/i_can_eat_glass.txt', 'rb')
     decoder = UTF8Decoder(decoder_fh, errors=REPLACE)
     for c in decoder:
-        _assertEqual(c, original_fh.read(1))
+        if c == EOF:
+            break
+        assertEqual(c, original_fh.read(1))
 
 ###############################################################################
 
-def run_tests():
-    # Run all global functions with a name that starts with "test_".
-    fn_cls = type(run_tests)
-    for k, v in sorted(globals().items()):
-        if k.startswith('test_') and isinstance(v, fn_cls):
-            test_name = v.__name__[5:]
-            stdout.write('testing {}'.format(test_name))
-            stdout.flush()
-            try:
-                v()
-            except AssertionError as e:
-                stdout.write(' - FAILED\n')
-                raise
-            except Skip:
-                stdout.write(' - SKIPPED\n')
-            else:
-                stdout.write(' - ok\n')
-            finally:
-                stdout.flush()
-
 if __name__ == '__main__':
-    run_tests()
+    cli(globals())
